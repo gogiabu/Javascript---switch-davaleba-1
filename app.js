b28310b9dedb89f5b36c23b3fd073630625536af
gogiabu <146556307+gogiabu@users.noreply.github.com>
2024-04-25 21:54:09 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+var age=18;
+switch(age){
+    case 10:
+        document.write ("არასწორია");
+        break;
+    case 25:
+        document.write ("არასწორია");
+        break;
+    case 18:
+        document.write ("სწორია");
+        break;    
+
+
+    
+
+
+
+
+}
